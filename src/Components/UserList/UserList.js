1fcc42eb3ed8e3262383dcf8a5a42c66d7ed6b8a
Seth Hill <sethjoshuahill@gmail.com>
2021-04-27 14:50:01 -0600

    Created the component folder with each component and their respective Css files

---
+++ src/Components/UserList/UserList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import './UserList.css'
+
+class UserList extends Component{
+
+
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+    
+}
+export default UserList
