3f08743224990b4133c257071fe5153dd596842e
Talon Walz <78514227+talonwalz@users.noreply.github.com>
2021-04-28 11:08:20 -0600

    header and routes setup

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,13 +1,31 @@
-import React from 'react'
-import './Header.css'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { logoutUser } from '../../redux/userReducer';
+import { connect } from 'react-redux';
+import axios from 'axios';
+import './Header.css';
 
-function Header(props){
+const Header = (props) => {
 
+    function logout() {
+        axios.delete('/auth/logout')
+        .then(() => {
+            props.logoutUser()
+            props.history.push('/')
+        })
+        .catch(err => console.log(err))
+    }
 
     return(
-        <div>
-
-        </div>
+        <header>
+            <section>
+                <h1>Where Should I Go?</h1>
+            </section>
+            <section>
+                <Link to="/my-list"><button>My List</button></Link>
+                <button>Logout</button>
+            </section>
+        </header>
     )
 }
-export default Header
+export default connect(null, { logoutUser })(Header)
