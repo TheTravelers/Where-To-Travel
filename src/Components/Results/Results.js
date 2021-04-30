a3e3b311a15e0d9a4e30cd9d296af72ec5c7afcd
Seth Hill <sethjoshuahill@gmail.com>
2021-04-30 08:43:48 -0600

    Working on the mainframe of the Results.js component

--- src/Components/Results/Results.js
+++ src/Components/Results/Results.js
@@ -1,17 +1,69 @@
+import axios from 'axios'
 import React, { Component } from 'react'
 import Header from '../Header/Header'
+import {updateSavedDestinations} from '../../redux/destinationReducer'
 import './Results.scss'
-
+ 
 class Results extends Component{
-
-
-    render(){
-        return(
-            <div>
-
-            </div>
-        )
-    }
-    
+   constructor(props){
+       super(props)
+       this.state = {
+           results: {}
+       }
+   }
+   componentDidMount(){
+ 
+       axios.get(`api/destinations/${newYork}`)
+       .then(res => {
+           this.setState({ results: res.data })
+       }).catch(err => console.log(err))
+   }
+ 
+   searchButton = () => {
+       axios.get('api/destinations')
+       .then(res => {
+           this.setState({ results: res.data })
+       }).catch(err => console.log(err))
+   }
+ 
+   addToUserList = (cityName) => {
+       axios.post(`/userDestList/${this.props.user.user.user.user_id}`, {cityName, distance, population, waterFront, adultFriendly})
+       .then(res => {
+           this.props.updateSavedDestinations(res.data)
+       })
+   }
+ 
+ 
+  
+ 
+   render(){
+       return(
+           <div>
+               <header>
+                   <Header />
+               </header>
+               <div>
+                   {this.state.results.map((element, index) => {
+                       const {cityName, distance, population, waterFront, adultFriendly} = element
+                       return(
+                           <div key={index}>
+                               <h2>{element.cityName}</h2>
+                               <li>
+                                   <ul>{element.distance} miles from your current destination</ul>
+                                   <ul>Area: {element.population > 20000 ? 'Urban' : 'Rural'}</ul>
+                                   <ul>Near Waterfront: {element.waterFront === true ? 'Yes' : 'No'}</ul>
+                                   <ul>Adult Activities Nearby: {element.adultFriendly > 4 ? 'Yes' : 'No'}</ul>
+                               </li>
+                               <button onClick={this.addToUserList()}
+                                    >Add</button>
+                           </div>
+                       )
+                   })}
+               </div>
+ 
+           </div>
+       )
+   }
+  
 }
 export default Results
--- src/redux/destinationReducer.js
+++ src/redux/destinationReducer.js
@@ -1,23 +1,35 @@
 const initialState = {
-    userSavedDestinations: ''
-}
-
-const GET_SAVED_DESTINATIONS = 'GET_SAVED_DESTINATIONS'
-
-export function getSavedDestinations(itemObj){
+    userSavedDestinations: []
+ }
+  
+ const GET_SAVED_DESTINATIONS = 'GET_SAVED_DESTINATIONS'
+ const UPDATE_SAVED_DESTINATIONS = 'UPDATE_SAVED_DESTINATIONS'
+  
+ export function getSavedDestinations(itemObj){
     return {
         type: GET_SAVED_DESTINATIONS,
         payload: itemObj
     }
-}
-
-export default function reducer(state= initialState, action){
+ }
+ export function updateSavedDestinations(itemObj){
+    return{
+        type: UPDATE_SAVED_DESTINATIONS,
+        payload: itemObj
+    }
+ }
+  
+ export default function reducer(state= initialState, action){
     switch(action.type){
         case GET_SAVED_DESTINATIONS:
             return{
                 ...state,
                 userSavedDestinations: action.payload
             }
+        case UPDATE_SAVED_DESTINATIONS:
+            return{
+                ...state, userSavedDestinations: [...state.arr, action.payload]
+            }
         default: return state;
     }
-}
+ }
+ 
